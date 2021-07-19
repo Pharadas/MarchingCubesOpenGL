fbc5732bf2efc6efc4f348c6d37215c7cc7e564a
Pharadas <davidsudo18@gmail.com>
2021-07-19 00:10:56 -0500

    added backface culling to initial setup

--- include/setup.h
+++ include/setup.h
@@ -39,6 +39,10 @@ class Setup {
 				glfwTerminate();
 			}
 
+			// glEnable(GL_CULL_FACE);
+			// glCullFace(GL_BACK);
+			// glFrontFace(GL_CW);  
+
 			glEnable(GL_DEPTH_TEST);
 
 			return window;
